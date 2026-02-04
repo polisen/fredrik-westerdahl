8f024a578ea467c48de85ee9f910a7d222a692ae
Fredrik Westerdahl <fredrik@megatech.ltd>
2026-02-04 13:00:40 +0100

    jjj

--- components/IntroCardLines.tsx
+++ components/IntroCardLines.tsx
@@ -64,7 +64,7 @@ export function IntroCardLines() {
           </div>
         </CardItem>
       </CardLine>
-
+{/* 
       <CardLine
         backgroundColor={getIntroCardLineColor(1)}
         alwaysVisibleCount={5}
@@ -107,7 +107,7 @@ export function IntroCardLines() {
           </div>
         </CardItem>
      
-      </CardLine>
+      </CardLine> */}
     </>
   );
 }
